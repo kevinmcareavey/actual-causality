d4a47503a78ed82dc3254e4caa6211eb0d7680a9
Kevin McAreavey <kevin.mcareavey@gmail.com>
2021-08-11 15:26:15 +0100

    Updated voting example
    Updated voting example to use string values and vote counting.

--- examples/voting.py
+++ examples/voting.py
@@ -1,31 +1,42 @@
 from frozendict import frozendict
 from actualcausality.boolean_combinations import PrimitiveEvent
 from actualcausality.hp_definition import Variable, CausalNetwork, CausalSetting, find_actual_causes, degrees_of_responsibility
-U_V1, U_V2, U_V3 = Variable("U_V1"), Variable("U_V2"), Variable("U_V3")
-V1, V2, V3, W = Variable("V1"), Variable("V2"), Variable("V3"), Variable("W")
-exogenous_variables = {U_V1, U_V2, U_V3}
+num_voters = 4
+U = [Variable(f"U_V{i+1}") for i in range(num_voters)]
+V, W = [Variable(f"V{i+1}") for i in range(num_voters)], Variable("W")
+exogenous_variables = set(U)
 endogenous_domains = {
-    V1: {False, True},
-    V2: {False, True},
-    V3: {False, True},
-    W: {False, True}
+    **{v: {"Suzy", "Billy"} for v in V},
+    W: {"Suzy", "Billy", "tie"}
 }
 causal_network = CausalNetwork()
-causal_network.add_dependency(V1, [U_V1], lambda parent_values: parent_values[U_V1])
-causal_network.add_dependency(V2, [U_V2], lambda parent_values: parent_values[U_V2])
-causal_network.add_dependency(V3, [U_V3], lambda parent_values: parent_values[U_V3])
-causal_network.add_dependency(W, [V1, V2, V3], lambda parent_values: ((parent_values[V1] and parent_values[V2]) or (parent_values[V1] and parent_values[V3])) or (parent_values[V2] and parent_values[V3]))
-context = {U_V1: True, U_V2: True, U_V3: True}
+for i in range(num_voters):
+    structural_equation = (lambda i: lambda parent_values: parent_values[U[i]])(i)  # WARNING: https://stackoverflow.com/questions/19837486/lambda-in-a-loop
+    causal_network.add_dependency(V[i], [U[i]], structural_equation)
+causal_network.add_dependency(
+    W,
+    V,
+    lambda parent_values:
+        "Suzy" if len([v for v in V if parent_values[v] == "Suzy"]) > (num_voters / 2) else "Billy" if len([v for v in V if parent_values[v] == "Billy"]) > (num_voters / 2) else "tie"
+)
+context = {
+    **{u: "Suzy" for u in U[:num_voters//2]},
+    **{u: "Billy" for u in U[num_voters//2:]}
+}
 causal_setting = CausalSetting(causal_network, context, endogenous_domains)
-event = PrimitiveEvent(W, True)
-# list(find_actual_causes(event, causal_setting))
-# degrees_of_responsibility(event, causal_setting)
+event = PrimitiveEvent(W, causal_setting.values[W])
+list(find_actual_causes(event, causal_setting))
+degrees_of_responsibility(event, causal_setting)
 
 causal_network.write("voting.png")
 
 actual_causes = {frozendict(actual_cause) for actual_cause in find_actual_causes(event, causal_setting)}
-expected_causes = [{V1: True, V2: True}, {V1: True, V3: True}, {V2: True, V3: True}, {W: True}]
+expected_causes = [{v: "Suzy"} for v in V[:num_voters//2]] + [{v: "Billy"} for v in V[num_voters//2:]] + [{W: 'tie'}]
 assert actual_causes == {frozendict(expected_cause) for expected_cause in expected_causes}
 actual_degrees_of_responsibility = degrees_of_responsibility(event, causal_setting)
-expected_degrees_of_responsibility = {V1: {True: 0.5, False: 0}, V2: {True: 0.5, False: 0}, V3: {True: 0.5, False: 0}, W: {True: 1.0, False: 0}}
+expected_degrees_of_responsibility = {
+    **{v: {"Billy": 0, "Suzy": 1.0} for v in V[:num_voters//2]},
+    **{v: {"Billy": 1.0, "Suzy": 0} for v in V[num_voters//2:]},
+    W: {"Suzy": 0, "tie": 1.0, "Billy": 0}
+}
 assert actual_degrees_of_responsibility == expected_degrees_of_responsibility
