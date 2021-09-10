b98bd317c243bdc9dfac468310e0710e57bc5853
Kevin McAreavey <kevin.mcareavey@gmail.com>
2021-09-10 16:53:00 +0100

    Added forest fire example explanations
    Added forest fire example explanations as per Example 7.1.2 from Halpern (2016).

--- examples/forest_fire_conjunctive.py
+++ examples/forest_fire_conjunctive.py
@@ -1,5 +1,7 @@
 from frozendict import frozendict
-from lib.halpern_pearl import Variable, CausalNetwork, CausalSetting, find_actual_causes, PrimitiveEvent
+from lib.halpern_pearl import Variable, CausalNetwork, CausalSetting, find_actual_causes, PrimitiveEvent, \
+    find_trivial_explanations, find_nontrivial_explanations, EpistemicState
+
 U_L, U_MD = Variable("U_L"), Variable("U_MD")
 FF, L, MD = Variable("FF"), Variable("L"), Variable("MD")
 exogenous_domains = {
@@ -25,3 +27,26 @@ causal_network.write("forest_fire_conjunctive.png")
 actual_causes = {frozendict(actual_cause) for actual_cause in find_actual_causes(event, causal_setting)}
 expected_causes = [{FF: True}, {L: True}, {MD: True}]
 assert actual_causes == {frozendict(expected_cause) for expected_cause in expected_causes}
+
+u0 = {U_L: False, U_MD: False}
+u1 = {U_L: True, U_MD: False}
+u2 = {U_L: False, U_MD: True}
+u3 = {U_L: True, U_MD: True}
+k1 = EpistemicState(causal_network, [u0, u1, u2, u3], exogenous_domains, endogenous_domains)
+k2 = EpistemicState(causal_network, [u0, u1, u2], exogenous_domains, endogenous_domains)
+k3 = EpistemicState(causal_network, [u0, u1, u3], exogenous_domains, endogenous_domains)
+k4 = EpistemicState(causal_network, [u1, u3], exogenous_domains, endogenous_domains)
+epistemic_states = [k1, k2, k3, k4]
+
+trivial_explanations = [{frozendict(trivial_explanation) for trivial_explanation in find_trivial_explanations(event, epistemic_state)} for epistemic_state in epistemic_states]
+expected_trivial_explanations = [
+    [{FF: True}, {L: True, MD: True}],
+    [],
+    [{FF: True}, {L: True, MD: True}],
+    [{FF: True}, {MD: True}]
+]
+assert trivial_explanations == [{frozendict(expected_trivial_explanation) for expected_trivial_explanation in epistemic_state} for epistemic_state in expected_trivial_explanations]
+
+nontrivial_explanations = [{frozendict(nontrivial_explanation) for nontrivial_explanation in find_nontrivial_explanations(event, epistemic_state)} for epistemic_state in epistemic_states]
+expected_nontrivial_explanations = [[] for _ in epistemic_states]
+assert nontrivial_explanations == [{frozendict(expected_nontrivial_explanation) for expected_nontrivial_explanation in epistemic_state} for epistemic_state in expected_nontrivial_explanations]
--- examples/forest_fire_disjunctive.py
+++ examples/forest_fire_disjunctive.py
@@ -1,6 +1,7 @@
 from frozendict import frozendict
-from lib.chockler_halpern import degrees_of_responsibility
-from lib.halpern_pearl import Variable, CausalNetwork, CausalSetting, find_actual_causes, CausalFormula, PrimitiveEvent, Negation
+from lib.halpern_pearl import Variable, CausalNetwork, CausalSetting, find_actual_causes, CausalFormula, PrimitiveEvent, \
+    Negation, find_trivial_explanations, EpistemicState, find_nontrivial_explanations
+
 U_L, U_MD = Variable("U_L"), Variable("U_MD")
 FF, L, MD = Variable("FF"), Variable("L"), Variable("MD")
 exogenous_domains = {
@@ -30,3 +31,31 @@ assert actual_causes == {frozendict(expected_cause) for expected_cause in expect
 assert CausalFormula({MD: False}, event).entailed_by(causal_setting)  # (Md, (1, 1)) |= [MD ← 0](FF = 1) example from Page 21 [Halpern, 2016]
 assert CausalFormula({L: False}, event).entailed_by(causal_setting)  # (Md, (1, 1)) |= [L ← 0](FF = 1) example from Page 21 [Halpern, 2016]
 assert CausalFormula({L: False, MD: False}, Negation(event)).entailed_by(causal_setting)  # (Md, (1, 1)) |= [L ← 0; MD ← 0](FF = 0) example from Page 21 [Halpern, 2016]
+
+u0 = {U_L: False, U_MD: False}
+u1 = {U_L: True, U_MD: False}
+u2 = {U_L: False, U_MD: True}
+u3 = {U_L: True, U_MD: True}
+k1 = EpistemicState(causal_network, [u0, u1, u2, u3], exogenous_domains, endogenous_domains)
+k2 = EpistemicState(causal_network, [u0, u1, u2], exogenous_domains, endogenous_domains)
+k3 = EpistemicState(causal_network, [u0, u1, u3], exogenous_domains, endogenous_domains)
+k4 = EpistemicState(causal_network, [u1, u3], exogenous_domains, endogenous_domains)
+epistemic_states = [k1, k2, k3, k4]
+
+trivial_explanations = [{frozendict(trivial_explanation) for trivial_explanation in find_trivial_explanations(event, epistemic_state)} for epistemic_state in epistemic_states]
+expected_trivial_explanations = [
+    [{FF: True}],
+    [{FF: True}],
+    [{FF: True}, {L: True}],
+    [{FF: True}, {L: True}]
+]
+assert trivial_explanations == [{frozendict(expected_trivial_explanation) for expected_trivial_explanation in epistemic_state} for epistemic_state in expected_trivial_explanations]
+
+nontrivial_explanations = [{frozendict(nontrivial_explanation) for nontrivial_explanation in find_nontrivial_explanations(event, epistemic_state)} for epistemic_state in epistemic_states]
+expected_nontrivial_explanations = [
+    [{L: True}, {MD: True}],
+    [{L: True}, {MD: True}],
+    [{MD: True}],
+    [{MD: True}]
+]
+assert nontrivial_explanations == [{frozendict(expected_nontrivial_explanation) for expected_nontrivial_explanation in epistemic_state} for epistemic_state in expected_nontrivial_explanations]
